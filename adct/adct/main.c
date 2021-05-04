60b98178a225a9313615624641e4d050e968305f
Robinlp <Robinlp@users.noreply.github.com>
2021-05-04 14:09:58 +0200

    dynamic delay has been implemented

--- adct/adct/main.c
+++ adct/adct/main.c
@@ -14,57 +14,64 @@
 #define ADC_CHANNEL (ADC_CHAN1)
 #define ADC_PIN (4)
 
-void TaskSample (void *pvParameters)
+void TaskSample(void *pvParameters)
 {
     ADC_CFG_Type cfg = {
-        .v18Sel=ADC_V18_SEL_1P82V,          // ADC 1.8V select
-        .v11Sel=ADC_V11_SEL_1P1V,           // ADC 1.1V select
-        .clkDiv=ADC_CLK_DIV_16,             // Clock divider
-        .gain1=ADC_PGA_GAIN_NONE,           // PGA gain 1
-        .gain2=ADC_PGA_GAIN_NONE,           // PGA gain 2
-        .chopMode=ADC_CHOP_MOD_AZ_PGA_ON,   // ADC chop mode select
-        .biasSel=ADC_BIAS_SEL_MAIN_BANDGAP, // ADC current form main or aon bandgap
-        .vcm=ADC_PGA_VCM_1V,                // ADC VCM value
-        .vref=ADC_VREF_3P2V,                // ADC voltage reference
-        .inputMode=ADC_INPUT_SINGLE_END,    // ADC input signal type
-        .resWidth=ADC_DATA_WIDTH_12,        // ADC resolution and oversample rate
-        .offsetCalibEn=0,                   // Offset calibration enable
-        .offsetCalibVal=0,                  // Offset calibration value
+        .v18Sel = ADC_V18_SEL_1P82V,          // ADC 1.8V select
+        .v11Sel = ADC_V11_SEL_1P1V,           // ADC 1.1V select
+        .clkDiv = ADC_CLK_DIV_16,             // Clock divider
+        .gain1 = ADC_PGA_GAIN_NONE,           // PGA gain 1
+        .gain2 = ADC_PGA_GAIN_NONE,           // PGA gain 2
+        .chopMode = ADC_CHOP_MOD_AZ_PGA_ON,   // ADC chop mode select
+        .biasSel = ADC_BIAS_SEL_MAIN_BANDGAP, // ADC current form main or aon bandgap
+        .vcm = ADC_PGA_VCM_1V,                // ADC VCM value
+        .vref = ADC_VREF_3P2V,                // ADC voltage reference
+        .inputMode = ADC_INPUT_SINGLE_END,    // ADC input signal type
+        .resWidth = ADC_DATA_WIDTH_12,        // ADC resolution and oversample rate
+        .offsetCalibEn = 0,                   // Offset calibration enable
+        .offsetCalibVal = 0,                  // Offset calibration value
     };
     ADC_FIFO_Cfg_Type fifo_cfg = {
         .fifoThreshold = ADC_FIFO_THRESHOLD_1, // ADC FIFO threshold
         .dmaEn = DISABLE,                      // ADC DMA enable
     };
-    
+
     ADC_Reset();
     ADC_Disable();
     ADC_Enable();
     ADC_Init(&cfg);
     ADC_Channel_Config(ADC_CHANNEL, ADC_CHAN_GND, 0);
     ADC_FIFO_Cfg(&fifo_cfg);
-    
-    bl_rtc_init();
-    
-    uint32_t startTime = bl_rtc_get_timestamp_ms(); // nyt
-    uint32_t i = 0; // nyt
 
-    while (1) {
+    bl_rtc_init();
+
+    //We set a start time and an iterator i to 0
+    uint32_t startTime = bl_rtc_get_timestamp_ms();
+    uint32_t i = 0;
+
+    while (1)
+    {
         ADC_Start();
         while (ADC_Get_FIFO_Count() == 0)
             ;
         uint32_t regval = ADC_Read_FIFO();
         uint32_t t = bl_rtc_get_timestamp_ms();
-        
-        // from ADC_Parse_Result
-        unsigned int val = (unsigned int)(((regval&0xffff)>>4)/1);
-        
-        uint32_t INPUT_DELAY = 100; //fix til dynamisk
 
-        vTaskDelay(INPUT_DELAY); // nyt
+        // from ADC_Parse_Result
+        unsigned int val = (unsigned int)(((regval & 0xffff) >> 4) / 1);
+
+        //calculate a delay based on the current time and iteration
+        uint32_t outputDelay = (100 * i) + (startTime - t);
+
+        //no delay if the calculated delay is 0 or less
+        if (outputDelay > 0){
+            vTaskDelay(outputDelay);
+        }
 
         printf("%u,%u\n", t, val);
 
-        i++; // nyt
+        //we increment the iterator variable
+        i++;
     }
 }
 
@@ -76,4 +83,3 @@ void bfl_main(void)
     xTaskCreate(TaskSample, "Blink", 1024, NULL, 1, NULL);
     vTaskStartScheduler();
 }
-
