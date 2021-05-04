d9196789021bb1dbd5abd5cb56b12bfbfda26a0b
CostaPrimo <jeppeenevold@gmail.com>
2021-05-04 14:14:02 +0200

    Python Test Files and results

---
+++ DynamicDelayTest.py
@@ -0,0 +1,22 @@
+import time
+if __name__ == '__main__':
+    i = 0  # Incrementer for delay calculation
+    times = []  # Placeholder for logged times
+    diffs = []  # Placeholder for diff between times
+    start = round(time.time()*1000)  # Start time
+    # Loop through 1000 times
+    while i <= 1000:
+        t = round(time.time()*1000)  # Get current time
+        delay = (100*i)+start-t  # Calculate delay
+        # Only delay if delay is greater than 0
+        if delay > 0:
+            time.sleep(delay / 1000)  # Do delay
+        times.append(round(time.time()*1000))  # Log time
+        print(i, delay, round(time.time()*1000))  # Print iteration, delay, and current time
+        i += 1  # Increment incrementer i
+    # For loop for calculating diffs between logged times
+    for x in range(len(times)):
+        # Skipping first element
+        if x > 0:
+            diffs.append(times[x]-times[x-1])  # Adding diff to the diffs placeholder
+    print(sum(diffs)/len(diffs))  # Print average diff between prints
